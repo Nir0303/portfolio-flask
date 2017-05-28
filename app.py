c26a44cfa74196717ad0106561bbf33f862e856d
Nir0303 <nbniranjan38@gmail.com>
2017-05-28 17:57:28 -0400

    Added changes

--- app.py
+++ app.py
@@ -6,4 +6,20 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-	return render_template('index.html')
+	return render_template('index.html',home=True)
+
+
+@app.route('/blog')
+def blog():
+	return render_template('blog.html',blog=True)
+
+
+@app.route('/project')
+def project():
+	return render_template('project.html',project=True)
+
+@app.route('/resume')
+def resume():
+	return send_file('static/files/Niranjan Addanki Resume.pdf')
+
+app.run()
