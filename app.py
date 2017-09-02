d6d1f68ba0f1086938b22fca2b110696bce8ef1c
nir0303 <nbniranjan38@gmail.com>
2017-09-02 16:17:16 -0400

    updated with latest profile

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask,render_template,url_for,request,session,redirect,send_file
 import os
+import argparse
 
 app = Flask(__name__)
 
+def parse():
+   parser=argparse.ArgumentParser()
+   parser.add_argument('--local',action='store_true',help='if mentioned run in local env')
+   args = parser.parse_args()
+   return args
+
 @app.route('/')
+@app.route('/main')
 @app.route('/index')
 def index():
 	return render_template('index.html',home=True)
@@ -15,6 +23,7 @@ def blog():
 
 
 @app.route('/project')
+@app.route('/projects')
 def project():
 	return render_template('project.html',project=True)
 
@@ -23,4 +32,8 @@ def resume():
 	return send_file('static/files/Niranjan Addanki Resume.pdf')
 
 if __name__ == '__main__':
-	app.run()
+        args = parse()
+	if args.local:
+           app.run(debug=True,host="0.0.0.0",port=8000)
+        else:
+           app.run()
